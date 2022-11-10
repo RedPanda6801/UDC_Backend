9c002182baa06341f64355a1d65674077a5f4df6
박승규 <redpanda6801@gmail.com>
2022-11-10 20:14:06 +0900

    DB 초기 설계 v1.0

---
+++ models/post.js
@@ -0,0 +1,47 @@
+// 지역 DB를 따로 두는 걸로 하자. 조회가 많기 때문
+const Sequelize = require("sequelize");
+
+module.exports = class Post extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        name: {
+          type: Sequelize.STRING(10),
+          allowNull: false,
+        },
+        email: {
+          type: Sequelize.STRING(50),
+          allowNull: false,
+        },
+        userId: {
+          type: Sequelize.STRING(15),
+          allowNull: false,
+        },
+        password: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+        },
+        nickname: {
+          type: Sequelize.STRING(8),
+          allowNull: false,
+        },
+        report: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "Post",
+        tableName: "Posts",
+        paranoid: true,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+
+  static associate(db) {}
+};
---
+++ models/user.js
@@ -0,0 +1,51 @@
+// 지역 DB를 따로 두는 걸로 하자. 조회가 많기 때문
+const Sequelize = require("sequelize");
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        name: {
+          type: Sequelize.STRING(10),
+          allowNull: false,
+        },
+        email: {
+          type: Sequelize.STRING(50),
+          allowNull: false,
+        },
+        userId: {
+          type: Sequelize.STRING(15),
+          allowNull: false,
+        },
+        password: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+        },
+        nickname: {
+          type: Sequelize.STRING(8),
+          allowNull: false,
+        },
+        report: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "User",
+        tableName: "Users",
+        paranoid: true,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+
+  static associate(db) {
+    db.User.hasMany(db.Cafe);
+    db.User.hasMany(db.Question);
+    db.User.hasMany(db.Solution);
+  }
+};
