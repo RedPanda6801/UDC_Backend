f0234b527e3198453a504aadbb4ed8ad071453db
박승규 <redpanda6801@gmail.com>
2022-11-22 17:46:16 +0900

    댓글 조회API 추가

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,7 +1,22 @@
+const { Post, Comment } = require("../models");
+
+// 게시글에 해당하는 모든 댓글을 불러오기
 exports.searchComment = async (req, res, next) => {
   try {
     // 받아온 데이터를 변수에 저장
     const { postId } = req.params;
+    // 게시글 있는지 확인
+    if (await Post.findOne({ where: { id: postId } })) {
+      console.error("Post is Not Found");
+      return res.status(404).json({
+        message: "Post is Not Found",
+      });
+    }
+    const comments = await Comment.findAll({ where: { PostId: postId } });
+    return res.status(200).json({
+      message: "Find Success",
+      data: comments,
+    });
   } catch (error) {
     console.error(error);
     next(error);
