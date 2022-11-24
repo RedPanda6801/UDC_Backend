48b8a6acafeee984011f861684c10b873bb193c0
uLikeSubak <sr05307@gmail.com>
2022-11-24 15:42:08 +0900

    auth v.1 add jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,47 +14,61 @@ exports.signUp = async(req, res, next)=>{
         const { name, email, userId, password, nickname } = req.body;
         const isUser = await User.findOne({where:{ userId }});
         if (!isUser){
+			const hash = await bcrypt.hash(password, 12);
             await User.create({
                 name,
                 email,
                 userId,
-                password,
+                password: hash,
                 nickname,
             })
             return res.Status(201).json({
-                message: "회원 가입 성공!",
+                message: "SignUp Success",
             });
         }else{
             res.Status(400).json({
-                message: "회원 정보를 확인 해주세요!"
+                message: "Check SignUp Info"
             })
         }
-        
     } catch (error) {
-        return res.Status(404).json({
-            
-        });
+       console.error(error);
+       next(error);
     }
-}
+};
 
 
 // for signIn
 
 
 exports.signIn = async(req, res, next)=>{
-    try {
-        const { userId, password } = req.body;
-        const validId = await User.findOne({where: userId })
-        if(validId){
-            if(password === validId.password){
-                return res.sendStatus(200);
-            }else{
-                return res.sendStatus(400);
-            }
-        }else{
-            return res.sendStatus(400);
-        }
-    } catch (error) {
-        return res.sendStatus(400);
-    }
+	try {
+		const { userId, password } = req.body;
+		const validId = await User.findOne({where:{
+			userId,
+		}})
+		if(userId){
+			const validPassword = await bcrypt.compare(password, validId.password);
+			if(validPassword){
+				const token = jwt.sign({
+					id: validId.id,
+					isAdmin: validId.isAdmin,
+				}, process.env.JWT_SECRET, {
+					expiresIn: '30000m', // for test  
+					issuer: 'UDC',
+				});
+				return res.status(200).json({
+				    message: 'token issued',
+				    token,
+				});
+			}
+			else{
+				return res.status(400).json({
+					message: 'Check Your Login Info'
+				})
+			}
+		} 
+	} catch (error) {
+		console.error(error);
+		next(error);
+	}	
 }
